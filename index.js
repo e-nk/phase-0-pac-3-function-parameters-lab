5c8cdb3e96b30106356150745195ec78a09a4748
e-nk <enocklagat747@gmail.com>
2022-11-17 23:49:13 +0300

    DONE WITH ASSIGNMENT

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name){
+    name === "Aki"
+    return(`Hi, my name is ${name}.`)
+    
+}
+function introductionWithLanguage(name, language){
+    name === "Aki"
+    language === "Ember.js"
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+    
+}
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    name === "Gracie" 
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+
+}
+
